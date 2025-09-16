cfd4cc617a20234e5c1bab4f52dee44fd7a1779f
Egor Matveev <wendli1999@mail.ru>
2025-09-16 18:05:52 +0300

    second_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import SetcionServices from "./components/section-services/SectionService"
+import SectionHeader from "./components/section-header/SectionHeader"
+import SectionRent from "./components/section-rent/SectionRent"
 
 function App() {
 
   return (
     <>
+      <SectionHeader />
       <SetcionServices />
+      <SectionRent />
+      
     </>
   )
 }
--- src/components/section-services/SectionService.jsx
+++ src/components/section-services/SectionService.jsx
@@ -3,7 +3,7 @@ import classes from './SectionService.module.css'
 
 export default function SetcionServices() {
     return (
-        <section className={classes.services}>
+        <section className={classes.services} id="services">
             <Service title="Годовое ТО" background="#22356F" />
             <Service title="Выравнивание колес" background="#0052C1" /> 
             <Service title="Настройка переключателей" background="#76B58B" />
